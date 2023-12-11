435c10f882989a5194f0ee0563d7c8b1ff57a820
DmytroSemeniuk <mr.semenyuk.dmitro@gmail.com>
2023-12-10 22:28:16 -0600

    validation to email input form added

--- HelperJobby/client-app/src/Components/AuthComponent/AuthComponent.tsx
+++ HelperJobby/client-app/src/Components/AuthComponent/AuthComponent.tsx
@@ -1,12 +1,44 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 import './AuthComponent.scss'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import {faArrowRightLong, faCircleExclamation} from '@fortawesome/free-solid-svg-icons';
 
 interface AuthComponentProps {}
 
 const AuthComponent: FC<AuthComponentProps> = () => {
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+    const [isSubmitInvalid, setIsSubmitInvalid] = useState(false);
+    const ValidateEmail = (email : string) : boolean => {
+        let result : boolean = true;
+        const atSignIndex : number = email.lastIndexOf('@');
+        if (!(atSignIndex > 0 && atSignIndex < email.lastIndexOf('.') && atSignIndex === email.indexOf('@') 
+        && email.length - atSignIndex > 2))
+        {
+            result = false;
+        }
+        
+        if (email.length < 5 || email.length > 50)
+            result = false;
+        return result;
+    }
     
+    let isFormInvalid = email.trim() === '';
+
+    const onFormSubmit = (e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        
+        if (!ValidateEmail(email)){
+            setError("Error: Invalid email address\n")
+            setIsSubmitInvalid(true);
+            isFormInvalid = true;
+        }
+        else 
+        {
+            setError("")
+        }
+    }
+
     return (
         <div className="container">
             <div className="passpage-container">
@@ -21,20 +53,31 @@ const AuthComponent: FC<AuthComponentProps> = () => {
                         <div className="email-form-subtitle">
                             <span className="form-subtitle">Create an account or sign in.</span>
                         </div>
-                        <div className="email-input-box">
+                        <form className="email-input-box" onSubmit={onFormSubmit}>
                             <div className="input-label-box">
-                                <label className="input-label">Email address
+                                <label className={`input-label ${isSubmitInvalid ? 'invalid-text' : ''}`}>Email address
                                     <span className="required-mark"> *</span>
                                 </label>
                             </div>
-                            <div className="input-box">
-                                <input className='email-input-field' />
+                            <div className={`input-box`}>
+                                <input className={`email-input-field ${isSubmitInvalid ? 'invalid-input-border' : ''}`} value={email} type="text"
+                                       onChange={(e) => {setEmail(e.target.value);
+                                       setError("");
+                                       setIsSubmitInvalid(false)}}/>
+                                <div className={"error-box"}>
+                                    {isSubmitInvalid && 
+                                    <>
+                                        <FontAwesomeIcon className={`error-text error-svg`} icon={faCircleExclamation} />
+                                        <span className={`error-text`}>{error}</span>
+                                    </>
+                                    }
+                                </div>
                             </div>
-                            <button className="submit-button">
+                            <button className="submit-button" type={"submit"} disabled={isFormInvalid}>
                                 <span className="continue-text">Continue</span>
                                 <FontAwesomeIcon className="continue-arrow" icon={faArrowRightLong} />
                             </button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
