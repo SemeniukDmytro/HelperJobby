3d4796cdc986be0369acf2003506b5f53b960161
DmytroSemeniuk <mr.semenyuk.dmitro@gmail.com>
2023-12-27 23:58:37 -0600

    detailed-description-component animations on scroll added

--- HelperJobby/client-app/src/Components/HomePage/HomePage.tsx
+++ HelperJobby/client-app/src/Components/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import PublicHomePageHeader from "../PublicHomePageHeader/PublicHomePageHeader";
 import "./HomePage.scss";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -14,11 +14,65 @@ interface HomePageProps {}
 
 const HomePage: FC<HomePageProps> = () => {
 
+    const [stickyHeight, setStickyHeight] = useState(361.2);
+    const mainContentRef = useRef<HTMLDivElement | null>(null);
+    const jobDetailsScrollWindowRef = useRef<HTMLDivElement | null>(null);
+    const [fullHeaderInfoHeight, setFullHeaderInfoHeight] = useState("100%");
+    const [shortHeaderInfoHeight, setShortHeaderInfoHeight] = useState("0");
+    const [fullHeaderInfoOpacity, setFullHeaderInfoOpacity] = useState(1);
+    const [shortHeaderInfoOpacity, setShortHeaderInfoOpacity] = useState(0);
+    const updateStickyHeight = () => {
+        const newHeight = 361.2 + window.scrollY;
+        if (newHeight > 698){
+            setStickyHeight(698)
+        }
+        else{
+            setStickyHeight(newHeight);
+        }
+    };
+    
+    useEffect(() => {
+        updateStickyHeight();
+        const handleScroll = () => {
+            updateStickyHeight();
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [stickyHeight]);
+
+    function focusOnInnerContent() {
+        if (mainContentRef.current) {
+            const mainContentY = mainContentRef.current.getBoundingClientRect().top + window.scrollY;
+            window.scrollTo(0, mainContentY);
+        }
+        if (jobDetailsScrollWindowRef.current){
+            const scrollTop = jobDetailsScrollWindowRef.current?.scrollTop;
+            if (scrollTop !== 0){
+                setFullHeaderInfoHeight("0");
+                setFullHeaderInfoOpacity(0);
+                setShortHeaderInfoHeight("100%");
+                setShortHeaderInfoOpacity(1);
+            }
+            else {
+                setFullHeaderInfoHeight("100%");
+                setFullHeaderInfoOpacity(1);
+                setShortHeaderInfoHeight("0");
+                setShortHeaderInfoOpacity(0);
+            }
+            
+        }
+        
+        
+    }
+
     return (
         <div className={"job-search-layout"}>
             <PublicHomePageHeader></PublicHomePageHeader>
             <div className={"header-and-main-content-separator"}></div>
-            <div className={"main-content"}>
+            <div className={"main-content"}  ref={mainContentRef}>
                 <JobSearchBar/>
                 <JobSearchPromoContainer/>
                 <div className={"search-results-container"}>
@@ -41,21 +95,28 @@ const HomePage: FC<HomePageProps> = () => {
                             <ShortJobDescriptionBlock></ShortJobDescriptionBlock>
                         </div>
                         <div className={"detailed-description-column"}>
-                            <div className={"detailed-description-sticky"}>
+                            <div style={{height : `${stickyHeight}px`}} className={"detailed-description-sticky"}>
                                 <div className={"detailed-description-content-box"}>
                                     <div className={"job-header-container"}>
                                         <div className={"job-header-title"}>
                                             <span className={"title-text"}>Job title</span>
                                         </div>
-                                        <a className={"header-company-name"}>
-                                            <span>DataAnnotations</span>
-                                        </a>
-                                        <div className={"header-job-location"}>
-                                            <span>Remote</span>
-                                        </div>
-                                        <div className={"header-job-salary"}>
-                                            <span>$27–$34 an hour</span>
-                                        </div>
+                                            <div className={"full-header-info"} style={{height : fullHeaderInfoHeight,
+                                            opacity : fullHeaderInfoOpacity}}>
+                                                <a className={"header-company-name"}>
+                                                    <span>DataAnnotations</span>
+                                                </a>
+                                                <div className={"header-job-location"}>
+                                                    <span>Remote</span>
+                                                </div>
+                                                <div className={"header-job-salary"}>
+                                                    <span>$27–$34 an hour</span>
+                                                </div>
+                                            </div>
+                                            <div className={"short-header-info" } style={{height : shortHeaderInfoHeight,
+                                            opacity : shortHeaderInfoOpacity}}>
+                                                <a className={"short-company-name"}>Company name |</a>
+                                            </div>
                                         <div className={"header-job-interactions-box"}>
                                             <button className={"apply-button"}>
                                                 Apply now
@@ -65,8 +126,11 @@ const HomePage: FC<HomePageProps> = () => {
                                             </button>
                                         </div>
                                     </div>
-                                    <div>
-
+                                    <div className={"job-details-scroll-window"} onScroll={focusOnInnerContent} ref={jobDetailsScrollWindowRef}>
+                                        <div className={"job-details-container"}>
+                                            <div className={"job-details-header"}>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
