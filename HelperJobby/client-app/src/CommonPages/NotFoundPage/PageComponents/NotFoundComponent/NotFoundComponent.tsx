f1701039b665d35b29cedb4a007f2aee78885251
DmytroSemeniuk <mr.semenyuk.dmitro@gmail.com>
2024-03-17 13:43:25 -0500

    page not found page added

--- HelperJobby/client-app/src/CommonPages/NotFoundPage/PageComponents/NotFoundComponent/NotFoundComponent.tsx
+++ HelperJobby/client-app/src/CommonPages/NotFoundPage/PageComponents/NotFoundComponent/NotFoundComponent.tsx
@@ -1,13 +1,50 @@
 import React, {FC} from 'react';
 import './NotFoundComponent.scss';
+import {faArrowRightLong} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useEmployer} from "../../../../hooks/contextHooks/useEmployer";
+import {useNavigate} from "react-router-dom";
+import EmployerPagesPaths from "../../../../AppRoutes/Paths/EmployerPagesPaths";
+import NoResults from "../../../../Components/Icons/NoResults";
 
 interface NotFoundComponentProps {
 }
 
-const NotFoundComponent: FC<NotFoundComponentProps> = () => (
-    <div className="NotFoundComponent">
-        Page not found
-    </div>
-);
+const NotFoundComponent: FC<NotFoundComponentProps> = () => {
+
+    const navigate = useNavigate();
+    const {employer} = useEmployer()
+    function navigateToHomePage() {
+        if (employer){
+            navigate(EmployerPagesPaths.JOB_POSTING);
+        }
+        else {
+            navigate("/");
+        }
+    }
+
+    return (
+        <div className={"not-found-page-container"}>
+            <NoResults/>
+            <div style={{maxWidth : "480px"}}>
+                <div className="form-title mb05rem">
+                    Lost? Let’s get you back on track.
+                </div>
+                <span
+                    className={"light-dark-default-text not-found-page-subtitle"}>
+                    We can’t find the page you’re looking for. The page might have moved, or there could be a typo in the URL.
+                </span>
+                <button
+                    onClick={navigateToHomePage}
+                    className={"blue-button mt1rem"}>
+                    Return to home
+                    <FontAwesomeIcon
+                        className={"svg1rem ml05rem"}
+                        icon={faArrowRightLong}/>
+                </button>
+            </div>
+        </div>
+    )
+};
 
 export default NotFoundComponent;
